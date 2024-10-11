f7481325123e2488bf285a976d1909122039d883
Lorena Medeiros <lomedeiros@proton.me>
2024-10-10 22:16:05 -0300

    feat: add actions column to CourseListComponent to add, edit and delete courses

--- src/app/components/courses-list/courses-list.component.ts
+++ src/app/components/courses-list/courses-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { Course } from '../../models/Course';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-courses-list',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatButtonModule, MatIconModule, MatTableModule],
   templateUrl: './courses-list.component.html',
   styleUrl: './courses-list.component.scss',
 })
 export class CoursesListComponent {
   @Input() courses: Course[] = [];
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 }
