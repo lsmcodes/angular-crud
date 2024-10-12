d4a6b4f5be7949d2e3dafedee8ee1921eac412bd
Lorena Medeiros <lomedeiros@proton.me>
2024-10-12 15:03:23 -0300

    feat: add CategoryPipe to determine the icon for each course category

--- src/app/components/courses-list/courses-list.component.ts
+++ src/app/components/courses-list/courses-list.component.ts
@@ -1,14 +1,15 @@
 import { Component, inject, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
-import { Course } from '../../models/Course';
-import { MatButtonModule } from '@angular/material/button';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Course } from '../../models/Course';
+import { CategoryPipe } from '../../pipes/category.pipe';
 
 @Component({
   selector: 'app-courses-list',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatTableModule],
+  imports: [MatButtonModule, MatIconModule, MatTableModule, CategoryPipe],
   templateUrl: './courses-list.component.html',
   styleUrl: './courses-list.component.scss',
 })
